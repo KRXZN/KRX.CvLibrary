acc32170569cab887f6f87c43d17a3ee5fbc85d6
Mitsuha9527 <547035646@qq.com>
2026-08-31 16:21:02 +0800

    增加ROI框对于图像的范围限制扩展函数

---
+++ src/CvLibrary/Extensions/CvRectExtension.cs
@@ -0,0 +1,19 @@
+﻿using CvCommon;
+
+namespace CvLibrary.Extensions
+{
+    public static class CvRectExtension
+    {
+        public static CvRect ClampRectToImage(this CvRect rect, int imageWidth, int imageHeight)
+        {
+            double x = Math.Max(0, rect.X);
+            double y = Math.Max(0, rect.Y);
+            double right = Math.Min(rect.Right, imageWidth);
+            double bottom = Math.Min(rect.Bottom, imageHeight);
+            double w = Math.Max(0, right - x);
+            double h = Math.Max(0, bottom - y);
+            return new CvRect(x, y, w, h);
+        }
+
+    }
+}
